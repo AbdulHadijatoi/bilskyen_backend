e9cc33767acdc64e67e370af1a87db62f9f1bf37
Abdul Hadi <abdulhadijatoi@gmail.com>
2025-12-21 07:34:20 +0500

    updated app service provider to show jwt message while secret is empty instead of showing error

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,21 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Validate JWT_SECRET is set, especially in production
+        $jwtSecret = config('jwt.secret');
+        if (empty($jwtSecret)) {
+            if (app()->environment('production')) {
+                throw new \RuntimeException(
+                    'JWT_SECRET is not set in production environment. ' .
+                    'Please run: php artisan jwt:secret to generate a secret key, ' .
+                    'then add it to your .env file as JWT_SECRET=your_generated_secret'
+                );
+            } else {
+                // In development, provide a helpful warning
+                \Log::warning(
+                    'JWT_SECRET is not set. Please run: php artisan jwt:secret to generate a secret key.'
+                );
+            }
+        }
     }
 }
