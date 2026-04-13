86d6659ff1135767d62b615c4c35eddb60d34626
Abdul Hadi <abdulhadijatoi@gmail.com>
2026-04-13 23:00:30 +0500

    fixes

--- app/Http/Controllers/AdminDmrDriveEnergyController.php
+++ app/Http/Controllers/AdminDmrDriveEnergyController.php
@@ -4,20 +4,13 @@ namespace App\Http\Controllers;
 
 use App\Models\DmrDriveEnergy;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
 
 class AdminDmrDriveEnergyController extends Controller
 {
-    public function index(Request $request): JsonResponse
+    public function index(): JsonResponse
     {
-        $limit = (int) $request->get('limit', 200);
-        if ($limit <= 0) {
-            $limit = 200;
-        }
-
         $items = DmrDriveEnergy::query()
             ->orderBy('name')
-            ->limit($limit)
             ->get(['id', 'type_nummer', 'name']);
 
         return $this->success($items);
